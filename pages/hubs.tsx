661612cb0a0921380472bed5ae338c243c2a4f2d
Gor Rustamyan <gor.rustamyan@gmail.com>
2021-03-31 18:27:54 +0400

    Added authentication.

--- pages/hubs.tsx
+++ pages/hubs.tsx
@@ -2,27 +2,27 @@ import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 
 export default function Hubs({ records }) {
-  return(
-    
-<div>
-  <h1>Community Weekly</h1>
-  <div>
-  {records.map((record) => (
-    <div key={record.id}>
-      <h2>
-        {record.fields.name}
-      </h2>
-      <p>
-        {record.fields.short_description}
-      </p>
-      <button className="bg-indigo-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-      <Link href={`/hub/[id]`} as={`/hub/${record.fields.slug}`}>
-            Read profile
+  return (
+
+    <div>
+      <h1>Community Weekly</h1>
+      <div>
+        {records.map((record) => (
+          <div key={record.id}>
+            <h2>
+              {record.fields.name}
+            </h2>
+            <p>
+              {record.fields.short_description}
+            </p>
+            <button className="bg-indigo-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              <Link href={`/hub/[id]`} as={`/hub/${record.fields.slug}`}>
+                Read profile
       </Link>
-      </button>
-    </div>))}
-  </div>
-</div>
+            </button>
+          </div>))}
+      </div>
+    </div>
   )
 }
 
@@ -30,12 +30,12 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   let headers = new Headers();
   headers.append("Authorization", "Bearer " + process.env.AIRTABLE_API);
   let records = []
-  let response = 
-  await fetch('https://api.airtable.com/v0/appN0b1jz6irTGYdC/Founders', {
-    method: 'GET',
-    headers: headers
-  });
+  let response =
+    await fetch('https://api.airtable.com/v0/appN0b1jz6irTGYdC/Founders', {
+      method: 'GET',
+      headers: headers
+    });
   let posts = await response.json();
   records = posts.records;
-return { props: { records } }
+  return { props: { records } }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,77 +7,77 @@ export default function Home({ records, episodes }) {
   return (
     <div>
       <Head>
-        <title>{records[0].fields.name} - podcast</title>
+        <title>{records.lenght > 0 ? records[0].fields.name : "No records"} - podcast</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="w-1/2 m-auto">
-      {records.map((record) => (
-        <div key={record.id}>
-          <h1 className="text-4xl p-4">
-            {record.fields.name}
-          </h1>
-          <p className="p-4">
-            {record.fields.description}
-          </p>
-          <div>
-            <ul className=" flex flex-row self-center">
-              <li className="p-4">
-                <h2>Follow us on</h2>
-              </li>
-              <li className="p-4"> 
-                <Link href={record.fields.twitter}>
-                  <a>
-                    <Image
-                      src="/twitter.png"
-                      alt="Picture of the author"
-                      width={30}
-                      height={30}>
-                  </Image>
-                  </a>
-                </Link>
-              </li>
-              <li className="p-4">
-                <Link href={record.fields.linkedin}>
-                  <a>
-                    <Image
-                      src="/linkedin.png"
-                      alt="Picture of the author"
-                      width={30}
-                      height={30}>
-                    </Image>
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>))}
+        {records.map((record) => (
+          <div key={record.id}>
+            <h1 className="text-4xl p-4">
+              {record.fields.name}
+            </h1>
+            <p className="p-4">
+              {record.fields.description}
+            </p>
+            <div>
+              <ul className=" flex flex-row self-center">
+                <li className="p-4">
+                  <h2>Follow us on</h2>
+                </li>
+                <li className="p-4">
+                  <Link href={record.fields.twitter}>
+                    <a>
+                      <Image
+                        src="/twitter.png"
+                        alt="Picture of the author"
+                        width={30}
+                        height={30}>
+                      </Image>
+                    </a>
+                  </Link>
+                </li>
+                <li className="p-4">
+                  <Link href={record.fields.linkedin}>
+                    <a>
+                      <Image
+                        src="/linkedin.png"
+                        alt="Picture of the author"
+                        width={30}
+                        height={30}>
+                      </Image>
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>))}
 
         {episodes.map((episode) => (
-        <div key={episode.id}>
-          <h1 className="text-2xl p-4">
-            {episode.fields.episode_name}
-          </h1>
-          <p className="p-4">
-            {episode.fields.short_description}
-          </p>
-         <div className="p-4">
-         <button className="bg-indigo-400 hover:bg-indigo-600 p-2">
-            <Link href={{
-                        pathname: '/episode/[id]',
-                        query: { id: episode.fields.slug },
-                    }}>
-              Listen
+          <div key={episode.id}>
+            <h1 className="text-2xl p-4">
+              {episode.fields.episode_name}
+            </h1>
+            <p className="p-4">
+              {episode.fields.short_description}
+            </p>
+            <div className="p-4">
+              <button className="bg-indigo-400 hover:bg-indigo-600 p-2">
+                <Link href={{
+                  pathname: '/episode/[id]',
+                  query: { id: episode.fields.slug },
+                }}>
+                  Listen
             </Link>
-          </button>
-         </div>
-        </div>))}
+              </button>
+            </div>
+          </div>))}
       </div>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  let headers = new Headers();
+  /*let headers = new Headers();
   headers.append("Authorization", "Bearer " + process.env.AIRTABLE_API);
   let records: any[];
   records = [];
@@ -103,5 +103,11 @@ return { props:
     { records: records.records,
       episodes: episodes.records,
     } 
+  }*/
+  return {
+    props: {
+      records: [],
+      episodes: []
+    }
   }
 }
